a597519cc12efff2a79fb050438c9e8634b8dd8f
gledsonfagno <gleidsonfagner838@gmail.com>
2024-06-20 13:39:18 -0300

    :white_check_mark: exercico autenticação via Token

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,10 +1,20 @@
 from rest_framework.viewsets import ModelViewSet
 
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication #added
+from rest_framework.permissions import IsAuthenticated #added
+
 from order.models import Order
 from order.serializers import OrderSerializer
 
 
 class OrderViewSet(ModelViewSet):
+
+  authentication_classes = [
+    SessionAuthentication, # added
+    BasicAuthentication, # added
+    TokenAuthentication, # added
+  ] 
+  permission_classes = [IsAuthenticated]  # added 
 
   serializer_class = OrderSerializer
   queryset = Order.objects.all().order_by('id')
